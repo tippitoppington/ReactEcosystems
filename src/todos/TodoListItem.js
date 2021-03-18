22274208c9a9076badd5709d6ade1ff11d5c6b0b
tippitoppington <meredith.k.duncan@gmail.com>
2021-03-18 05:18:26 -0400

    added mark as complete thunk again

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -12,7 +12,7 @@ const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
         </button>
       )}
       <button
-        onClick={() => onRemovePressed(todo.text)}
+        onClick={() => onRemovePressed(todo.id)}
         className='remove-button'>
         Remove
       </button>
--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,5 +1,6 @@
 import {
   createTodo,
+  removeTodo,
   loadTodosInProgress,
   loadTodosSuccess,
   loadTodosFailure,
@@ -29,6 +30,18 @@ export const addTodoRequest = (text) => async (dispatch) => {
     });
     const todo = await response.json();
     dispatch(createTodo(todo));
+  } catch (e) {
+    dispatch(displayAlert(e));
+  }
+};
+
+export const removeTodoRequest = (id) => async (dispatch) => {
+  try {
+    const response = await fetch(`http://localhost:8080/todos/${id}`, {
+      method: 'delete',
+    });
+    const removedTodo = await response.json();
+    dispatch(removeTodo(removedTodo));
   } catch (e) {
     dispatch(displayAlert(e));
   }
